6f2578ea47ae0151849c05c1ba968dbce3857a29
Xuemei Huang <mei.huang9500@gmail.com>
2024-10-11 16:54:20 +0900

    set up redux

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,17 +1,43 @@
+"use client";
+import { useEffect } from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  // レンダリング後に実行される
+  //　第二引数でuseeffectの中身を実行するタイミングを指定できる
+  // 第二引数は空の配列だとリロード時の一回のみ、変数を入れると変数の値が変わった時に実行することもできる
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
   return (
     <div className="bg flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
-      sidebar
+      <Sidebar />
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 ${isSidebarCollapsed ? "" : "md:pl-64"}`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
