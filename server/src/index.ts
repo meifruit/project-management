42914db15106974a508cbb56a3d292d55a23097b
Xuemei Huang <mei.huang9500@gmail.com>
2024-10-27 19:53:41 +0900

    add projects route

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import projectRoutes from "./routes/projectRoutes";
 /* ROUTE IMPORTS */
 // import projectRoutes from "./routes/projectRoutes";
 // import taskRoutes from "./routes/taskRoutes";
@@ -27,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("This is home route");
 });
 
-// app.use("/projects", projectRoutes);
+app.use("/projects", projectRoutes);
 // app.use("/tasks", taskRoutes);
 // app.use("/search", searchRoutes);
 // app.use("/users", userRoutes);
