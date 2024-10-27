9831d3ca63d9f1c948e7e0849488a29d98801bd9
Xuemei Huang <mei.huang9500@gmail.com>
2024-10-27 23:25:07 +0900

    add create and delete project api

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getProjects } from "../controllers/projectController";
+import {
+  createProject,
+  deleteProject,
+  getProjects,
+} from "../controllers/projectController";
 
 const router = Router();
 
 router.get("/", getProjects);
-
+router.post("/", createProject);
+router.delete("/", deleteProject);
 export default router;
