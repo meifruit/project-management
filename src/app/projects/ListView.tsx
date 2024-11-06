9a4821c14feeeb43228fb18f7d27d6b93d58dc3a
Xuemei Huang <mei.huang9500@gmail.com>
2024-11-05 21:07:58 -0800

    add taskcard

--- src/app/projects/ListView.tsx
+++ src/app/projects/ListView.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
-import { useGetTasksQuery } from "@/state/api";
+import TaskCard from "@/components/TaskCard";
+import { Task, useGetTasksQuery } from "@/state/api";
 
 type ListProps = {
   id: string;
@@ -19,8 +20,11 @@ const ListView = ({ id, setIsModalNewTaskOpen }: ListProps) => {
   if (error) return <div>An error occurred while fetching tasks </div>;
   return (
     <div className="px-4 pb-8 xl:px-6">
-      <div className="pt-5">
+      {/* <div className="pt-5">
         <Header name="List" />
+      </div> */}
+      <div className="">
+        {tasks?.map((task: Task) => <TaskCard key={task.id} task={task} />)}
       </div>
     </div>
   );
